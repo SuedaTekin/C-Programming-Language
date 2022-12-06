efa557a17eccc48619d48074ce575b343cd5190a
Sueda Tekin <115072432+SuedaTekin@users.noreply.github.com>
2022-12-06 15:24:36 +0300

    Add files via upload

---
+++ Lab1/2.soru.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int yaricap,pi=3.14,alan;
+	printf("yaricap:");
+	scanf("%d",&yaricap);
+	
+	alan=(pi*yaricap*yaricap);
+	        printf("alan:%d",alan);
+	    
+	
+	
+	
+	
+	
+	return 0;
+}
---
+++ Lab1/4.soru.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int dna,dnu,sicaklik;
+	printf("sýcaklik:");
+	scanf("%d",&sicaklik);
+	
+	
+	if
+	("sicaklik>0");
+	printf("dnu:");
+	
+    }
+    
+	else
+	
+	   printf ("dna") ;
+	
+	 /*end-else*/
+    
+	
+	
+	
+	
+	return 0;
+}
