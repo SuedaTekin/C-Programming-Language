395eadaccf0a582058b83916bf9f7969b65a7897
Sueda Tekin <115072432+SuedaTekin@users.noreply.github.com>
2022-12-28 15:49:00 +0300

    Add files via upload

---
+++ Lab1/lab12(1.soru).c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main()
+{
+char str1[]="Ahmet";
+printf ("%s",str1);
+
+
+char str2[]="Ali";
+printf("%s",str2);
+
+	return 0;
+}
---
+++ Lab1/lab12(2.soru).c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int i;
+	char str1[20]="p2'r-o@gram84iz./";
+	for(i=0;i<20;i++){
+		if (str1[i]>='a'  &&  str1[i]<='z')
+			printf("%c",str1[i]);
+		
+	}
+		
+		
+	
+	
+	
+	return 0;
+}
